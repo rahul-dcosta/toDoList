0860706b37e190484ea98574016d0db5962bc257
Rahul D'Costa <rdcosta@umich.edu>
2020-05-10 22:52:07 +0400

    setting state

--- App.js
+++ App.js
@@ -9,16 +9,32 @@ import Footer from "./components/Footer"
 */
 import './style.css'
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
     super()
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState =>{
+      const updatedTodos = prevState.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
   }
   
   render() {
-    const todoItems = this.state.todos.map(item => <ToDoItem key = {item.id} item ={item} />)
+    const todoItems = this.state.todos.map(item => <ToDoItem key = {item.id} item ={item}
+    handleChange={this.handleChange} />)
     return (
       <div className="todo-list">
           {todoItems}
